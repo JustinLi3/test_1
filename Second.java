9460bc605284966db97bae71cc7e691d345d1deb
JustinLi3 <Triamethyest@gmail.com>
2023-12-09 23:06:00 -0500

    Practice Again

--- Second.java
+++ Second.java
@@ -1,6 +1,10 @@
 package test_1;
 public class Second{
     public static void main(String[] args){
-        System.out.print("hello this is practice");
+        System.out.print("hello this is practice"); 
+        for (int x = 0 ; x<32l;x++){
+            System.out.print("hi");
+        }
+        System.out.print("testing");
     }
 }
